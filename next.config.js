3d8a98d80b727c06eba1b9a0cb6201eb331a67db
Hisbelis Vargas <hisbelisv@gmail.com>
2025-03-05 21:40:37 -0600

    build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
+  basePath: "/astratta",
+  output: "export",
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
